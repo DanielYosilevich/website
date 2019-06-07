9b962faf49ea73b2d5e608683cf8e2cb289a744b
Daniel Yosilevich <danielyosilevich@gmail.com>
2019-06-07 10:29:40 +0300

    Updates

---
+++ precache-manifest.bc774c5fa3254abb0a832fc0fd2b7e34.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07ce6068836f2bdc08764115a86606f6",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "07b22bdb027d686846d4",
+    "url": "/website/static/css/main.4944a1bd.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/website/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "07b22bdb027d686846d4",
+    "url": "/website/static/js/main.78b7e336.chunk.js"
+  },
+  {
+    "revision": "d15da1c5dbbceb93f3a7",
+    "url": "/website/static/js/runtime~main.de462641.js"
+  }
+]);
